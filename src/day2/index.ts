aba79d603501095ebb509494ece7ea33bf963928
jake <jsmith181997@gmail.com>
2025-12-02 07:19:48 -0600

    add day 2 setup

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -1,1 +1,16 @@
-console.log("Day 2");
+import {readFileSync} from "fs";
+console.log("Day 2!");
+
+const data = readFileSync("resources/day-2-data.txt", "utf-8");
+const lines = data.trim().split(/,/);
+
+console.log(lines);
+console.log(`processed data: ${lines.length} lines`);
+
+
+for (const line of lines) {
+    // in each range look for invalid ids
+    // (repeating numbers)
+
+    // add the sum of all the invalid ids
+}
