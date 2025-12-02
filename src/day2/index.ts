a4191b7f51834aa772a177218b4d60ad8a36fd0c
Jacob Smith <jacob.w.smith97.mil@army.mil>
2025-12-02 13:30:08 -0600

    update tsconfig, complete day 2 part 1

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -1,16 +1,32 @@
 import {readFileSync} from "fs";
 console.log("Day 2!");
 
-const data = readFileSync("resources/day-2-data.txt", "utf-8");
+const data = readFileSync("./resources/day-2-data.txt", "utf-8");
 const lines = data.trim().split(/,/);
 
 console.log(lines);
 console.log(`processed data: ${lines.length} lines`);
 
+let sumInvalidIds = 0;
 
 for (const line of lines) {
     // in each range look for invalid ids
     // (repeating numbers)
+    const [start, end] = line.split(/-/);
+    const startIndex = Number(start);
+    const endIndex = Number(end);
 
-    // add the sum of all the invalid ids
+    for (let i = startIndex; i < endIndex; i++) {
+        const currentNumber = i.toString();
+        const front = currentNumber.substring(0, currentNumber.length / 2);
+        const back = currentNumber.substring(currentNumber.length / 2);
+
+        if (front === back) {
+            console.log(`Found invalid id: ${currentNumber}`)
+            sumInvalidIds += i;
+        }
+    }
+
 }
+
+console.log(`Sum of invalid ids: ${sumInvalidIds}`);
